1d23be6d99f0f04f6e03e881b38bf23aac4f4445
22p31a0534 <119798543+mouniyasheik@users.noreply.github.com>
2022-12-19 21:46:00 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     float b,sc,t;     if(n<=199)     {         b=n*1.20;     }     else if(n>=200&&n<400)     {     b=n*1.50;     }     else if(n>=400&&n<600)     {         b=n*1.80;     }     else     {     b=n*2.00;     }         if(b>400)         {             sc=b*0.15;             t=sc+b;         }         else         {             sc=100;             t=sc+b;         }         printf("%.2f",t);
    }

---
+++ Current_bill.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    float b,sc,t;
+    if(n<=199)
+    {
+        b=n*1.20;
+    }
+    else if(n>=200&&n<400)
+    {
+    b=n*1.50;
+    }
+    else if(n>=400&&n<600)
+    {
+        b=n*1.80;
+    }
+    else
+    {
+    b=n*2.00;
+    }
+        if(b>400)
+        {
+            sc=b*0.15;
+            t=sc+b;
+        }
+        else
+        {
+            sc=100;
+            t=sc+b;
+        }
+        printf("%.2f",t);
+    
+}
