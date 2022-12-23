f0c087288f403078e5622f001c274c0501dd2890
22p31a0534 <119798543+mouniyasheik@users.noreply.github.com>
2022-12-24 03:58:43 +0530

    #include<stdio.h> int main() {     int x,i;     scanf("%d",&x);     for(i=1;i<=x;i++)     {         int a,b,c;         scanf("%d%d",&a,&b);         c=a+b;         printf("%d ",c);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int x,i;
+    scanf("%d",&x);
+    for(i=1;i<=x;i++)
+    {
+        int a,b,c;
+        scanf("%d%d",&a,&b);
+        c=a+b;
+        printf("%d
+",c);
+    }
+}
