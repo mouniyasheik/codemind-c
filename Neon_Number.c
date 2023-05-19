e03fc722341259acf5a964f05470a8abf25d8c25
22p31a0534 <119798543+mouniyasheik@users.noreply.github.com>
2023-05-19 20:27:37 +0530

    #include<stdio.h> int main() {     int a,s,sum=0,t,r;     scanf("%d",&a);     s=a*a;     t=s;     while(t!=0)     {         r=t%10;         sum=sum+r;         t/=10;     }     if(sum==a)     {         printf("Neon Number");     }     else{         printf("Not Neon Number");     }
    }

--- Neon_Number.c
+++ Neon_Number.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,q,r,s=0;
-    scanf("%d",&n);
-    i=n*n;
-    q=i;
-    while(q!=0)
+    int a,s,sum=0,t,r;
+    scanf("%d",&a);
+    s=a*a;
+    t=s;
+    while(t!=0)
     {
-      
-       r=q%10;
-       s=s+r;
-       q=q/10;
+        r=t%10;
+        sum=sum+r;
+        t/=10;
     }
-    if(n==s)
-    printf("Neon Number");
-    else
-    printf("Not Neon Number");
+    if(sum==a)
+    {
+        printf("Neon Number");
+    }
+    else{
+        printf("Not Neon Number");
+    }
+        
 }
