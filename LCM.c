4fdecfdeae37ad70dc1778ed96c1a11bc96dab26
22p31a0534 <119798543+mouniyasheik@users.noreply.github.com>
2023-05-19 20:20:08 +0530

    #include<stdio.h> int main() {     int a,b,i;     scanf("%d%d",&a,&b);     for (i=1;i<=b;i++)     {         if(i*a%b==0)         {             printf("%d",a*i);             break;         }     } }

--- LCM.c
+++ LCM.c
@@ -3,12 +3,12 @@ int main()
 {
     int a,b,i;
     scanf("%d%d",&a,&b);
-    for(i=1;i<=b;i++)
+    for (i=1;i<=b;i++)
     {
         if(i*a%b==0)
         {
-        printf("%d",i*a);
-        break;
+            printf("%d",a*i);
+            break;
         }
     }
 }
