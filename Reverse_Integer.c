b50d3288e064ad5677ca1d61b59eb9a79c3abfd1
22p31a0534 <119798543+mouniyasheik@users.noreply.github.com>
2023-06-10 13:11:16 +0530

    #include<stdio.h> int main() {     int n,i,q,r,s=0;     scanf("%d",&n);     q=n;     while(q!=0)     {        r=q%10;        s=s*10+r;        q=q/10;     }     printf("%d",s); }

--- Reverse_Integer.c
+++ Reverse_Integer.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int n,r,s=0,q;
+    int n,i,q,r,s=0;
     scanf("%d",&n);
     q=n;
     while(q!=0)
     {
-        r=q%10;
-        s=s*10+r;
-        q/=10;
+       r=q%10;
+       s=s*10+r;
+       q=q/10;
     }
     printf("%d",s);
 }
