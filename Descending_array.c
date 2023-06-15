dc58212db76681871fbe7f9a32c824e2c2b1000d
22p31a0534 <119798543+mouniyasheik@users.noreply.github.com>
2023-06-15 21:31:27 +0530

    #include<stdio.h> int main() {     int n,i=0,s=1;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n-1;i++)     {         if (a[i]<a[i+1])         {             s=0;             break;         }     }    if (s==0) {        printf("no");    }    else{        printf("yes");    }
    }

---
+++ Descending_array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=0,s=1;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        if (a[i]<a[i+1])
+        {
+            s=0;
+            break;
+        }
+    }
+   if (s==0) {
+       printf("no");
+   }
+   else{
+       printf("yes");
+   }
+    
+}
